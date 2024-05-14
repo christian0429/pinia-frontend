32aa9776ffa2400fe59ea206719d2a4d0870e41f
christian0429 <christianmellor429@gmail.com>
2024-05-14 17:27:31 +0900

    Add files via upload

---
+++ 0527-vue3-app/basic-q4-vue-project2/src/components/ProductBox.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="center">
+    <hr>
+    <br>
+    <table align="center">
+      <tr>
+        <td class="td">제품번호</td>
+        <td class="td2">100</td>
+      </tr>
+      <tr>
+        <td class="td">제품이름</td>
+        <td class="td2">
+          <a href="product?no=100">shoes1</a>
+        </td>
+      </tr>
+      <tr>
+        <td class="td">제품설명</td>
+        <td class="td2">fun shoes1</td>
+      </tr>
+      <tr>
+        <td class="td">제품가격</td>
+        <td class="td2">1000</td>
+      </tr>
+      <tr>
+        <td class="td">제품회사</td>
+        <td class="td2">c100</td>
+      </tr>
+      <tr>
+        <td class="td">제품이미지</td>
+        <td class="td2">
+          <img src="../assets/img/c100.png" width="100" height="100">
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <img src="../assets/img/basket.png" width="300" height="200">
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+td {
+  width: 200px;
+}
+.td {
+  background: #0dcaf0;
+}
+.td2 {
+  background: #badbcc;
+}
+</style>
